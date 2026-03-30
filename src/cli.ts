ab2333dac1936a8aa9907e51199c9972a3196fb4
syy-shark <jinlin1868@gmail.com>
2026-03-30 18:40:36 +0800

    chore: bump version to 0.3.3

--- src/cli.ts
+++ src/cli.ts
@@ -237,7 +237,7 @@ async function main() {
   }
 
   if (args[0] === "--version" || args[0] === "-v") {
-    console.log("sharkcode v0.3.2");
+    console.log("sharkcode v0.3.3");
     return;
   }
 
